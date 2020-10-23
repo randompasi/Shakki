6f53db02b89754bb6381ee35daf7e6f872a7c812
randomapsi <anmivu@utu.fi>
2020-10-24 01:38:46 +0300

    removing unnessary mewthods from bishop

--- src/main/java/ChessPieces/Bishop.java
+++ src/main/java/ChessPieces/Bishop.java
@@ -13,19 +13,13 @@ public class Bishop extends Piece implements Serializable{
 		super(colour, coordinate);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public boolean isMovePossible(int fromX, int fromY, int toX, int toY){
-		//diagonal move
-		if(Math.abs(toX - fromX) == Math.abs(toY - fromY)){
+	@Override
+	public boolean isMovePossible(Coordinate toCoordinate){
+
+		if(Math.abs(toCoordinate.getX() - fromX) == Math.abs(toCoordinate.getY() - fromY)){
             return true;
 		}
 		return false;
-	}
-
-	@Override
-	public boolean isMovePossible(Coordinate toCoordinate) {
-
-	return 	isMovePossible(fromX, fromY, toCoordinate.getX(),toCoordinate.getY());
 	}
 
 	@Override
